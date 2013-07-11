4012f3f1d076701937e85206b94310192753f83e
Paulo R. Macedo Chaves <proberto.macedo@gmail.com>
2013-07-11 15:38:34 -0300

    Fixes get_prep_value

--- youtubeurl_field/modelfields.py
+++ youtubeurl_field/modelfields.py
@@ -1,31 +1,33 @@
-import urlparse
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
 from django.db import models
-from django import  forms
-from django.utils.translation import ugettext_lazy as _ 
-from django.utils import six
+from django.utils.translation import ugettext_lazy as _
 
 from youtubeurl_field import formfields
 from youtubeurl_field.validators import validate_youtube_url
 from youtubeurl_field.youtubeurl import YoutubeUrl
- 
+
+
 class YoutubeUrlField(models.CharField):
     __metaclass__ = models.SubfieldBase
     description = _("YouTube url")
-    
+
     def __init__(self, *args, **kwargs):
         super(YoutubeUrlField, self).__init__(*args, **kwargs)
         self.validators = (validate_youtube_url,)
 
     def get_internal_type(self):
         return "CharField"
-    
+
     def to_python(self, value):
+        print 'to_python', repr(value)
         if isinstance(value, YoutubeUrl):
             return value
         return YoutubeUrl(value)
 
     def get_prep_value(self, value):
-        return self.to_python(value)
+        return value.value
 
     def formfield(self, **kwargs):
         defaults = {
@@ -34,7 +36,7 @@ class YoutubeUrlField(models.CharField):
         defaults.update(kwargs)
         return super(YoutubeUrlField, self).formfield(**defaults)
 
- 
+
 try:
     from south.modelsinspector import add_introspection_rules
     add_introspection_rules([
